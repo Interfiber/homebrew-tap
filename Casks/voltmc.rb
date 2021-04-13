ae7d1736a31567873fe2807c4cab5d807894e5aa
Interfiber <hunterstasonis@icloud.com>
2021-04-13 17:40:55 -0400

    :sparkles: Add casks

---
+++ Casks/voltmc.rb
@@ -0,0 +1,9 @@
+cask "voltmc" do
+    version "0.0.1"
+    sha256 "ad67f2d2a65711078eb8f762501198d53750b09864ccee9b4bf862a827c030e7"
+    url "https://github.com/Interfiber/volt/releases/download/v0.0.1/Volt.dmg"
+    name "Volt"
+    homepage "https://interfiber.github.io/volt"
+  
+    app "Volt.app"
+  end
