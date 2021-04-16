b66e5b3af7b2f07316f0e5953a3a721b3a8db098
Interfiber <hunterstasonis@icloud.com>
2021-04-16 18:27:55 -0400

    :package: Update package voltmc

--- Casks/voltmc.rb
+++ Casks/voltmc.rb
@@ -1,7 +1,7 @@
 cask "voltmc" do
-    version "0.0.1"
-    sha256 "ad67f2d2a65711078eb8f762501198d53750b09864ccee9b4bf862a827c030e7"
-    url "https://github.com/Interfiber/volt/releases/download/v0.0.1/Volt.dmg"
+    version "1.0"
+    sha256 "f4074dabe3298ccd8d90c7c23a20fcb6f2f56ceed403e9b39a7a9b864a6c5e05"
+    url "https://github.com/Interfiber/volt/releases/download/v1.0/Volt.dmg"
     name "Volt"
     homepage "https://interfiber.github.io/volt"
   
