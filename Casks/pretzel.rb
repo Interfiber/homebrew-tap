a40b6850908b4e97c7d77fa955c7aa17acf9949b
AshPerson <webmaster@interfiber.dev>
2021-10-11 15:48:09 -0400

    Update pretzel.rb

--- Casks/pretzel.rb
+++ Casks/pretzel.rb
@@ -1,6 +1,6 @@
 cask "pretzel" do
     version "1.0"
-    sha256 "af319571a55aa09c153ace98f1626a3c706bd8b75f8701917cb122a4df3ceae4"
+    sha256 "42d37d66703111eb02d4ccfce2efb8ff46608a55d74916bf2f9d7dd236337701"
     url "https://github.com/Interfiber/Pretzel/releases/download/v0.0.1/PretzelClient.app.zip"
     name "Pretzel"
     homepage "https://interfiber.github.io/pretzel"
