2ca5369093b02a9286fefa442476b1b411de6d4f
AshPerson <webmaster@interfiber.dev>
2021-10-11 11:26:53 -0400

    Create pretzel.rb

---
+++ Casks/pretzel.rb
@@ -0,0 +1,9 @@
+cask "pretzel" do
+    version "1.0"
+    sha256 "424c9f2253ff9684325616095cca01b21a8c57225714c9f2d28a59d7951cb34b"
+    url "https://github.com/Interfiber/Pretzel/releases/download/v0.0.1/PretzelClient.app.zip"
+    name "Pretzel"
+    homepage "https://interfiber.github.io/pretzel"
+  
+    app "PretzelClient.app"
+ end
