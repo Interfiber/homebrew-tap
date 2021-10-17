b7be9691670a12271d26a42bc4b1cf1b2fcc8fea
Interfiber <webmaster@interfiber.dev>
2021-10-17 14:01:25 -0400

    push

---
+++ Casks/pak.rb
@@ -0,0 +1,9 @@
+cask "pak" do
+    version "0.1"
+    sha256 "ae48fd4698287706afbd00e946f2f4788ff8c356246e3500f919cdf397d0324d"
+    url "https://github.com/Interfiber/pak/releases/download/v0.1/Pak.Installer.pkg"
+    name "Pretzel"
+    homepage "https://interfiber.github.io/pretzel"
+  
+    pkg "Pak.Installer.pkg"
+ end
--- Casks/pretzel.rb
+++
@@ -1,9 +0,0 @@
-cask "pretzel" do
-    version "1.0"
-    sha256 "42d37d66703111eb02d4ccfce2efb8ff46608a55d74916bf2f9d7dd236337701"
-    url "https://github.com/Interfiber/Pretzel/releases/download/v0.0.1/PretzelClient.app.zip"
-    name "Pretzel"
-    homepage "https://interfiber.github.io/pretzel"
-  
-    app "PretzelClient.app"
- end
